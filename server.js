710b6147cf3ec28eb8e7cdc46964015cd236ec89
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-08-17 14:21:23 -0500

    Update server.js
    add body-parser; start to create new code

--- server.js
+++ server.js
@@ -4,6 +4,11 @@
 // init project
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -18,12 +23,18 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
+/*
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+*/
 
+app.get("/api/:date", function (req, res) {
+  res.json({
+  // fill in rest here - stopped 08/17/2021
+  })
+})
 
 
 // listen for requests :)
