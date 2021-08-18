1b57bcd4a1159b2cad12ad8aa281f632a134a402
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-08-18 09:46:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get("/api/hello", function (req, res) {
 */
 
 app.get("/api/:date", (req, res) => {
-  res.send(JSON.parse(req.params.date)); 
+  res.send(req.params.date); 
   /*
   if (req.params >= -8.64e12 && req.params <= 8.64e15) {
     res.send(req.params);
