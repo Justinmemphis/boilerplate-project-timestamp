4b25cc931be6e5afff6ba933024b3a1a7fc73ee7
Justin Carter <63558266+Justinmemphis@users.noreply.github.com>
2021-08-18 14:43:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get("/api/hello", function (req, res) {
 */
 
 app.get("/api/:date", (req, res) => {
-  // res.send(req.params);  // this is what is breaking
+  res.send(req.params);
   /*
   if (req.params >= -8.64e12 && req.params <= 8.64e15) {
     res.send(req.params);
